215c5840de9934fd0b5ff4f9e79263c03ac53fd8
walexmania <90723024+walexmania@users.noreply.github.com>
2022-04-19 09:42:34 +0100

    initial
    commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>OLATUNJI ADEWALE ABDULWAHAB</h1>
+      <address className="addr">
+        <div>
+          <p>37, Olagoke Street,</p>
+          <p> Ikola Ilumo, Via Ipaja,</p>
+          <p>Lagos State</p>
+        </div>
+        <div>
+          <p>olatunji.adewale.a@gmail.com</p>
+          <p>0813-919-9147</p>
+        </div>
+      </address>
+    </div>
+  );
+}
+
+export default Header;
